54c38fe789632318597c84a7f5e30adf4dd54ac5
ERRrOR404 <error99998@gmail.com>
2021-09-16 14:24:28 +0900

    feat: install packages

--- md-swiper/src/App.js
+++ md-swiper/src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
+import { React, Component } from 'react';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import SwiperCore, { Navigation, Pagination, Autoplay } from "swiper";
+import styled from '@emotion/styled';
+import ReactMarkdown from 'react-markdown';
+import "swiper/swiper.scss";
+import "swiper/components/navigation/navigation.scss";
+import "swiper/components/pagination/pagination.scss";
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+    render() {
+        SwiperCore.use([Navigation, Pagination, Autoplay]);
+        const MarkdownStyle = styled.div`
+          font-size: 1em;
+          line-height: 2.5rem;
+        `;
+
+        const mdEx = `
+          **안녕하세용**
+        `;
+
+        return(
+            <div className="App">
+              <MarkdownStyle>
+                <ReactMarkdown>{mdEx}</ReactMarkdown>
+              </MarkdownStyle>
+
+              {/* <Swiper
+                      className="project-swiper"
+                      spaceBetween={500}
+                      slidesPerView={1}
+                      navigation
+                      pagination={{ clickable: true }}
+                      autoplay={{ delay: 2500 }}
+                >
+                  <SwiperSlide></SwiperSlide>
+                  <SwiperSlide>Hid</SwiperSlide>
+                  <SwiperSlide>Hidd</SwiperSlide>
+              </Swiper> */}
+            </div>
+        );
+    }
 }
 
 export default App;
